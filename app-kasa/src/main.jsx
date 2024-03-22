a36130a58def911f1ddcb16fc23bbb7399ecbc6d
BenjaminPrince <prince-benjamin@hotmail.com>
2024-03-22 15:18:25 +0100

    (app-kasa): add home + navbar/footer

--- app-kasa/src/main.jsx
+++ app-kasa/src/main.jsx
@@ -4,13 +4,18 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import "./index.css";
-import Root from "./routes/root";
+import "./kasa-home.css";
+import "./kasa-footer.css";
+import Root from "./routes/kasa-home";
+import ErrorPage from "./routes/error-page";
+import KasaFooter from "./routes/kasa-footer";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+    footer:<KasaFooter />,
   },
 ]);
 
