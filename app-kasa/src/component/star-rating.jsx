09add76fbb5cee0e2607fe9336d3fe0e0ebb5e74
BenjaminPrince <prince-benjamin@hotmail.com>
2024-05-01 14:48:53 +0200

    (app-kasa): correction maj

--- app-kasa/src/component/star-rating.jsx
+++ app-kasa/src/component/star-rating.jsx
@@ -1,24 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../css/stars.css'; // Importer le fichier CSS
 
-const StarRating = ({ initialRating, onRatingChange }) => {
-    const [rating, setRating] = useState(initialRating);
-
-    const handleClick = (index) => {
-        const newRating = index + 1;
-        setRating(newRating);
-        if (onRatingChange) {
-            onRatingChange(newRating);
-        }
-    };
-
+const StarRating = ({ initialRating }) => {
     return (
         <div className="star-rating">
             {[...Array(1)].map((_, index) => (
                 <span
                     key={index}
-                    className={index < rating ? 'star active' : 'star'} 
-                    onClick={() => handleClick(index)}
+                    className={index < initialRating ? 'star active' : 'star'} 
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" width="196" height="36" viewBox="0 0 196 36" fill="none">
                     <path d="M179.721 6.84366C179.473 6.32809 178.948 6 178.371 6C177.795 6 177.275 6.32809 177.022 6.84366L174.008 13.0445L167.277 14.0382C166.715 14.1225 166.246 14.5162 166.073 15.0552C165.9 15.5942 166.04 16.1895 166.443 16.5879L171.327 21.4201L170.174 28.2491C170.08 28.8115 170.315 29.3833 170.779 29.7161C171.243 30.0489 171.857 30.0911 172.363 29.8239L178.376 26.6133L184.389 29.8239C184.896 30.0911 185.51 30.0536 185.974 29.7161C186.438 29.3786 186.672 28.8115 186.578 28.2491L185.421 21.4201L190.304 16.5879C190.707 16.1895 190.853 15.5942 190.675 15.0552C190.496 14.5162 190.032 14.1225 189.47 14.0382L182.735 13.0445L179.721 6.84366Z" fill="#E3E3E3"/>
--- app-kasa/src/pages/lodging-page.jsx
+++ app-kasa/src/pages/lodging-page.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import accommodationsData from '../../public/logement.json';
 import Collapse from '../component/collapse';
@@ -11,23 +11,15 @@ import '../css/lodging.css';
 function LodgingPage() {
     const { id } = useParams();
     const lodging = accommodationsData.find(lodging => lodging.id === id);
-    const [rating, setRating] = useState(lodging.rating); // État local pour stocker la note
 
     if (!lodging)
         return <ErrorPage />;
     
-    const { title, location, tags, description, equipments, pictures, host } = lodging;
+    const { title, location, tags, description, equipments, pictures, host, rating } = lodging;
 
     const equipmentItems = equipments.map((equipment, index) => (
         <span key={index} className="equipment">{equipment}</span>
     ));
-
-    const handleRatingChange = (newRating) => {
-        // Met à jour la note dans l'état local
-        setRating(newRating);
-        // Mettre en œuvre la logique de mise à jour de la note dans votre application
-        // Vous pouvez envoyer la nouvelle note à votre API backend, par exemple
-    };
 
     return (
         <div className="lodging-page">
@@ -36,18 +28,18 @@ function LodgingPage() {
                 <h1 className="title">{title}</h1>
                 <p className="location">{location}</p>
 
-            <div className="tags">
-                {tags.map((tag, index) => (
-                    <span key={index} className="tag">{tag}</span>
-                ))}
-            </div>
+                <div className="tags">
+                    {tags.map((tag, index) => (
+                        <span key={index} className="tag">{tag}</span>
+                    ))}
+                </div>
             </div>
            
             <div className="container">
                 <div className="host-info">
                     <div className='name'>
-                    <p className="first-name">{host.name.split(' ')[0]}</p>
-                    <p className="last-name">{host.name.split(' ')[1]}</p>
+                        <p className="first-name">{host.name.split(' ')[0]}</p>
+                        <p className="last-name">{host.name.split(' ')[1]}</p>
                     </div>
                     <img src={host.picture} alt="Host" className="host-picture" />
                 </div>
